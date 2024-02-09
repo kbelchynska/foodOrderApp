12a778bd335687e38d897c9b8bc84c349560e889
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2024-02-09 15:38:37 +0200

    Add CartContext

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useReduser } from "react";
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => { },
+    removeItems: (id) => { },
+});
+
+const cartReduser = (state, action) => {
+    if (action.type === "ADD_ITEM") {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id,
+        );
+
+        const updatedItems = [...state.items];
+
+        if (existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
+    }
+
+    if (action.type === "REMOVE_ITEM") {
+    }
+
+    return state;
+};
+
+export const CartContextProvider = ({ children }) => {
+    useReduser(cartReduser, { items: [] });
+    return <CartContext.Provider>{children}</CartContext.Provider>;
+};
+
+export default CartContext;
