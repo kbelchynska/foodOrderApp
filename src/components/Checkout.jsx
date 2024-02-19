e7133df900dc0b436a59952c55dea413ebf05205
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2024-02-19 09:02:19 +0200

    Add submition

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -18,11 +18,16 @@ const Checkout = () => {
         userProgressCtx.hideCheckout();
     };
 
-    const handleSubmit = () => { };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const fd = FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+    };
 
     return (
         <Modal open={userProgressCtx.progress == "checkout"} onClose={handleClose}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
                 <Input label="Full Name" type="text" id="full-name" />
